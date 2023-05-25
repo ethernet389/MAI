66ce6b98c971b2f7f12cbea62dccc3bf884df3af
ethernet389 <etheret389@gmail.com>
2023-05-26 00:04:56 +0800

    Fix wrong recycler

---
+++ app/src/main/java/com/example/mai2/main_programme/activities/check_mai_activity/wrappers/ArrayPairList.java
@@ -0,0 +1,16 @@
+package com.example.mai2.main_programme.activities.check_mai_activity.wrappers;
+
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
+
+public class ArrayPairList {
+    public ArrayList<Pair<String, String>> list;
+    public ArrayPairList(){
+        list = new ArrayList<>();
+    }
+
+    public ArrayPairList(ArrayList<Pair<String, String>> list){
+        this.list = list;
+    }
+}
